976f9475487350d2c568d65adde4e47b893553a0
Anh-Tu Hoang <hatu87@gmail.com>
2015-12-15 23:49:23 +0700

    initial project

---
+++ XeemAPI/Controllers/UsersController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace XeemAPI.Controllers
+{
+    [RoutePrefix("api/users")]
+    public class UsersController : ApiController
+    {
+        [Route("authenticate")]
+        [HttpPost]
+        public IHttpActionResult Authenticate()
+        {
+            var request = HttpContext.Current.Request;
+            var email = request["email"];
+            var password = request["password"];
+
+            if (email == null || password == null)
+            {
+                return NotFound();
+            }
+
+            var token = Models.User.Authenticate(email, password);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(token);
+        }
+
+        // GET: api/Users
+        public IHttpActionResult Get()
+        {
+            var users = Models.User.FindAll();
+
+            if(users == null)
+            {
+                return InternalServerError();
+            }
+
+            return Ok(users);
+        }
+
+        // GET: api/Users/5
+        public IHttpActionResult Get(int id)
+        {
+            var user = Models.User.FindById(id);
+            if (user == null) {
+                return NotFound();
+            }
+
+            return Json(user);
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public IHttpActionResult Post(int id, [FromUri]Models.User user)
+        {
+            var updatedProperties = new List<string>();
+            if(user.Name != null)
+            {
+                updatedProperties.Add("name");
+            }
+
+            if(user.Phone != null)
+            {
+                updatedProperties.Add("phone");
+            }
+
+            if(user.Address != null)
+            {
+                updatedProperties.Add("address");
+            }
+
+            if(user.Birthday != null)
+            {
+                updatedProperties.Add("birthday");
+            }
+            
+            if(!Models.User.Update(user, updatedProperties))
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        // PUT: api/Users/5
+        [HttpPut]
+        public IHttpActionResult Put([FromBody]string email, [FromBody]string phone, [FromBody]string password)
+        {
+            var user = new Models.User();
+            user.Email = email;
+            user.Phone = phone;
+            user.Password = password;
+
+            if (!Models.User.Insert(user))
+            {
+                return InternalServerError();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ XeemAPI/Models/User.cs
@@ -0,0 +1,293 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace XeemAPI.Models
+{
+    [DataContract]
+    public class User
+    {
+        int id;
+        string name;
+        string email;
+        string password;
+        string phone;
+        string avatarUrl;
+        DateTime createdDate;
+        string address;
+        DateTime? birthday;
+        string latitude;
+        string longitude;
+
+        [DataMember]
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+        [DataMember]
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        [DataMember]
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+        [DataMember]
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+        [DataMember]
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+
+            set
+            {
+                phone = value;
+            }
+        }
+        [DataMember]
+        public string AvatarUrl
+        {
+            get
+            {
+                return avatarUrl;
+            }
+
+            set
+            {
+                avatarUrl = value;
+            }
+        }
+        [DataMember]
+        public DateTime CreatedDate
+        {
+            get
+            {
+                return createdDate;
+            }
+
+            set
+            {
+                createdDate = value;
+            }
+        }
+        [DataMember]
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+
+            set
+            {
+                address = value;
+            }
+        }
+        [DataMember]
+        public DateTime? Birthday
+        {
+            get
+            {
+                return birthday;
+            }
+
+            set
+            {
+                birthday = value;
+            }
+        }
+        [DataMember]
+        public string Latitude
+        {
+            get
+            {
+                return latitude;
+            }
+
+            set
+            {
+                latitude = value;
+            }
+        }
+        [DataMember]
+        public string Longitude
+        {
+            get
+            {
+                return longitude;
+            }
+
+            set
+            {
+                longitude = value;
+            }
+        }
+
+        public static explicit operator User(Data.User dto)
+        {
+            User user = new User();
+            user.id = dto.id;
+            user.latitude = dto.latitude;
+            user.longitude = dto.longitude;
+            user.name = dto.name;
+            user.password = dto.password;
+            user.phone = dto.phone;
+
+            return user;
+        }
+
+        public Data.User ExportToDataObject()
+        {
+            var dto = new Data.User();
+            dto.address = this.address;
+            dto.avatarUrl = this.avatarUrl;
+            dto.birthday = this.birthday;
+            dto.createdDate = this.createdDate;
+            dto.email = this.email;
+            dto.id = this.id;
+            dto.latitude = this.latitude;
+            dto.longitude = this.longitude;
+            dto.name = this.name;
+            dto.password = this.password;
+            dto.phone = this.phone;
+
+            return dto;
+        }
+
+        public static string Authenticate(string email, string password)
+        {
+            try
+            {
+                using (var model = new Data.XeemEntities())
+                {
+                    var q = from u in model.Users
+                            where u.email.Equals(email) && u.password.Equals(password)
+                            select u;
+
+                    return ((User)q.First<Data.User>()).id.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public static User[] FindAll()
+        {
+            try
+            {
+                using (var model = new Data.XeemEntities())
+                {
+                    return Array.ConvertAll(model.Users.ToArray(), item => (User)item);
+                }
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+            
+        }
+
+        public static User FindById(int id)
+        {
+            using (var model = new Data.XeemEntities())
+            {
+                var user = model.Users.Find(id);
+
+                if (user == null)
+                    return null;
+
+                return (User)user;
+            }
+        }
+
+        public static bool Insert(User user)
+        {
+            try
+            {
+                using (var model = new Data.XeemEntities())
+                {
+                    var dto = user.ExportToDataObject();
+                    model.Users.Add(dto);
+                    model.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool Update(User user, List<string> updatedProperties)
+        {
+            try {
+                using (var model = new Data.XeemEntities())
+                {
+                    var objectContext = ((IObjectContextAdapter)model).ObjectContext;
+                    var dto = user.ExportToDataObject();
+                    model.Users.Attach(dto);
+                    var entry = objectContext.ObjectStateManager.GetObjectStateEntry(dto);
+                    
+                    foreach(var columnName in updatedProperties)
+                    {
+                        entry.SetModifiedProperty(columnName);
+                    }
+
+                    model.SaveChanges();                    
+                }
+            }catch(Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
